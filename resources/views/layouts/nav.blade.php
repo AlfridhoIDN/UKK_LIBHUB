b2c89610a729c21417bef34e35c8e3b80a01b49a
AlfridhoIDN <luckyanandhaa@gmail.com>
2026-02-25 22:47:51 +0700

    add:favorite

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -39,7 +39,7 @@
                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
                                 Profil Saya
                             </a>
-                            <a href="#" class="flex items-center gap-3 px-4 py-2.5 text-sm font-bold text-emerald-800 hover:bg-emerald-50 transition">
+                            <a href="{{ route('user.fav') }}" class="flex items-center gap-3 px-4 py-2.5 text-sm font-bold text-emerald-800 hover:bg-emerald-50 transition">
                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path></svg>
                                 Favorit
                             </a>
